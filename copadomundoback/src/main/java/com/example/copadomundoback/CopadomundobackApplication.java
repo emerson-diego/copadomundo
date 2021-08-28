fce0bd75e300ef80baeb0de4e87488b78bf8ac14
Emerson <diegowebby@gmail.com>
2021-08-28 08:31:31 -0300

    first commit

---
+++ copadomundoback/src/main/java/com/example/copadomundoback/CopadomundobackApplication.java
@@ -0,0 +1,13 @@
+package com.example.copadomundoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopadomundobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopadomundobackApplication.class, args);
+	}
+
+}
---
+++ copadomundoback/src/test/java/com/example/copadomundoback/CopadomundobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copadomundoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopadomundobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
