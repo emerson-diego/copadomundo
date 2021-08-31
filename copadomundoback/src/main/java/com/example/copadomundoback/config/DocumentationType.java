00e8a8f83f610d6357f3572dfc14d4cbfa5415ae
Emerson <diegowebby@gmail.com>
2021-08-30 21:36:01 -0300

    fix: correção swagger

--- copadomundoback/src/main/java/com/example/copadomundoback/config/DocumentationType.java
+++
@@ -1,5 +0,0 @@
-package com.example.copadomundoback.config;
-
-public interface DocumentationType {
-
-}
--- copadomundoback/src/main/java/com/example/copadomundoback/config/SwaggerConfig.java
+++ copadomundoback/src/main/java/com/example/copadomundoback/config/SwaggerConfig.java
@@ -1,11 +1,21 @@
 package com.example.copadomundoback.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
@@ -14,16 +24,36 @@ import springfox.documentation.swagger.web.UiConfigurationBuilder;
 public class SwaggerConfig {
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.copadomundoback"))
-				// .apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().defaultModelsExpandDepth(Integer.valueOf(-1)).build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Meu Bolão Rest API", "Meu Bolão Rest API", "1.0", "",
+				new Contact("Emerson", "meubolao.com.br", "diegowebby@gmail.com"), "", "", Collections.emptyList());
 	}
 
 	@Bean
-	UiConfiguration uiConfig() {
-		return UiConfigurationBuilder.builder().defaultModelsExpandDepth(Integer.valueOf(-1)).build();
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+				.securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey())).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.copadomundoback")).paths(PathSelectors.any())
+				.build();
 	}
 
 }
